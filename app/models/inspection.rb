5029cf9ec6d89648cd5cbc1da2b7041c660d2b96
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-12-04 15:26:40 -0500

    Implement enums for Inspection

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,3 +1,14 @@
 class Inspection < ApplicationRecord
   belongs_to :restaurant
+
+  enum :inspection_type, [
+    "Routine - Unscheduled",
+    "Complaint",
+    "Reinspection/Followup",
+    "Foodborne Illness Investigation",
+    "Non-inspection site visit",
+    "Routine - Scheduled",
+    "New Ownership"
+  ]
+  enum :risk_category, ["Low Risk", "Moderate Risk", "High Risk"]
 end
